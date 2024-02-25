537f3329bf46f4200de2f95e1e826b957529af69
remoflury <remoflury97+web@gmail.com>
2024-02-25 08:53:30 +0100

    mountain animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
+	import Mountains from '$lib/components/assets/mountains.svelte';
 	import '../css/main.css';
 </script>
 
-<main class="min-h-dvh">
+<main class="min-h-dvh relative">
 	<slot />
+
+	<figure class="absolute bottom-0 left-0 right-0" role="presentation">
+		<Mountains />
+	</figure>
 </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent'
+      },
       container: {
         center: true,
         padding: {
